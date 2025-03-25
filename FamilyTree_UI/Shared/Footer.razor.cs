7e790674e349233946846821168be7623bcc591b
Suman Tandan <87357770+tandon1999@users.noreply.github.com>
2025-03-25 19:19:13 +0545

    Family Tree UI Update
    Family Tree UI Update

--- FamilyTree_UI/Shared/Footer.razor.cs
+++ FamilyTree_UI/Shared/Footer.razor.cs
@@ -1,7 +1,12 @@
+using Microsoft.JSInterop;
+
 namespace FamilyTree_UI.Shared
 {
     public partial class Footer
     {
-
+        private async Task ScrollToTop()
+        {
+            await JS.InvokeVoidAsync("scrollToTop");
+        }
     }
 }
---
+++ FamilyTree_UI/wwwroot/js/familyui.js
@@ -0,0 +1,22 @@
+﻿window.getElementPosition = (elementId) => {
+    const element = document.getElementById(elementId);
+    if (element) {
+        const rect = element.getBoundingClientRect();
+        return { x: rect.left + window.scrollX, y: rect.top + window.scrollY };
+    }
+    return { x: 0, y: 0 };
+};
+
+window.drawConnectorLine = (elementId, x, y, length, angle) => {
+    const element = document.getElementById(elementId);
+    if (element) {
+        element.style.left = `${x}px`;
+        element.style.top = `${y}px`;
+        element.style.width = `${length}px`;
+        element.style.transform = `rotate(${angle}deg)`;
+    }
+};
+
+window.getConnectorLine = (elementId) => {
+    return document.getElementById(elementId);
+};
